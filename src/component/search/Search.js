80f8979e4a7209e1bf43e3eee4cb331530ae0c2a
Yadirasarmientop <yadirasarmientop@gmail.com>
2021-07-18 20:40:08 -0500

    fix: Desarrollando el buscador con el curso

---
+++ src/component/search/Search.js
@@ -0,0 +1,29 @@
+import React from 'react';
+// import { useForm } from '../../Hooks/useForm';
+import '../inicio/MangaCard.css';
+
+export const Search = () => {
+
+    
+    return (
+        <>
+            <div className="search">
+                <h2 className="titulo">BUSCADOR DE MANGAS</h2>
+                <hr />
+
+                {/* <input
+                type = "text"
+                className = "search-bar"
+                placeholder = "Buscar..."
+                autoComplete = "off"
+                value = {search}
+                onChange = {handleInputChange}
+                ></input>
+                <div className= "btn icon-search">
+                <i className="fas fa-search"></i>
+                </div> */}
+               
+            </div>
+        </>
+    )
+}
--- src/component/search/SearchScreen.js
+++ src/component/search/SearchScreen.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import { useForm } from '../../Hooks/useForm';
+// import { useForm } from '../../Hooks/useForm';
 import '../ui/header.css';
+import { Search } from './Search';
 
 export const SearchScreen = () => {
 
-    const [{search}, handleInputChange]= useForm ({search: ''});
-    return (
-        <>
-            <div className="search">
-                <input
-                    type = "text"
-                    className = "search-bar"
-                    placeholder = "Buscar..."
-                    autoComplete = "off"
-                    value = {search}
-                    onChange = {handleInputChange}
-                ></input>
-                <div className= "btn icon-search">
-                    <i className="fas fa-search"></i>
-                </div>
-            </div>
-        </>
+    // const [{search}, handleInputChange]= useForm ({search: ''});
+    return ( <Search />
+        // <>
+        //     <div className="search">
+        //         <input
+        //             type = "text"
+        //             className = "search-bar"
+        //             placeholder = "Buscar..."
+        //             autoComplete = "off"
+        //             value = {search}
+        //             onChange = {handleInputChange}
+        //         ></input>
+        //         <div className= "btn icon-search">
+        //             <i className="fas fa-search"></i>
+        //         </div>
+        //     </div>
+        // </>
     )
 }
